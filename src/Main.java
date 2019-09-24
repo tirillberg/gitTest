25584fd45517cc961e81b858ea3774d6d49d3f83
sofiamohrnielsen <soni18af@student.cbs.dk>
2019-09-24 09:14:39 +0200

    changed username

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String myUsername = "FIA";
+        String myUsername = "pejrgeger";
         String myPassword = "password";
         String message = "Hello World";
 
